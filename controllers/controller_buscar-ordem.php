ac22ecfcf29b14dd04f364557f1fc6de25d3777c
guilhermetec2 <guilhermetec2@gmail.com>
2017-08-24 22:07:15 -0300

    Implementação do layout inicial

--- controllers/controller_buscar-ordem.php
+++
@@ -1,4 +0,0 @@
-<?php
-    
-    require_once __DIR__.'/../views/view_buscar-ordem.php';
-?>
---
+++ views/view_principal.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/w3.css">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    
+    <title>chatTXT</title>
+</head>
+<body>
+    <header>
+        <section class="top-bar w3-block">
+            <div class="w3-right"> USUARIO</div>
+        </section>
+    </header>
+    
+    <main>
+        <div class="area w3-margin-top">
+            <div class="w3-row">
+                <div class="container-esquerda w3-third w3-margin-right">
+                    <section class="container-contatos">
+                        CONTATOS
+                    </section>
+                </div><!-- fim container-esquerda -->
+                
+                <div class="container-direita w3-rest">
+                    <section class="container-mensagens">
+                        CONVERSA
+                    </section>
+
+                    <section class="container-editor">
+                        EDITOR    
+                    <!-- <div class="editor">
+                            <textarea name="" id=""></textarea>
+                        </div> -->
+                    </section>
+                </div><!-- fim container-direita -->
+            </div>
+        </div>
+    </main>  
+</body>
+</html>
